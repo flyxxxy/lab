ec81a09740559c81beb68ba8d94d96df5d707537
flyxxxy <56066813+flyxxxy@users.noreply.github.com>
2019-10-21 10:26:21 +0300

    Update lab2n2.cpp

--- lab2n2.cpp
+++ lab2n2.cpp
@@ -6,7 +6,7 @@ int main() {
 	int n = 0;
 	cout << "Enter a quantity of numbers: " << endl;
 	cin >> n;
-	int *arr = new int[n];
+	int *B = new int[n];
 	int max=0;
 	int min=0;
 	int result = 0;
@@ -21,14 +21,14 @@ int main() {
 	}
 	
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+		cin >> B[i];
 	}
 
 	for (int i = 0; i < n; i++) {
-		if (min > arr[i]) 
-			min = arr[i];
-		if (max < arr[i])
-			max = arr[i];
+		if (min > B[i]) 
+			min = B[i];
+		if (max < B[i])
+			max = B[i];
 	}
 
 	if (max == 0)
@@ -40,7 +40,7 @@ int main() {
 		cout << "Result: " << result << endl;
 	}
 	
-	delete[] arr;
+	delete[] B;
 	return 0;
 }
 
