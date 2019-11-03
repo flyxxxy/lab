d1bf30922d7cc14a0695572a05e6e2664257df19
flyxxxy <56066813+flyxxxy@users.noreply.github.com>
2019-11-03 22:36:27 +0300

    Update lab3n13.cpp

--- lab3n13.cpp
+++ lab3n13.cpp
@@ -1,41 +1,69 @@
 #include <iostream>
 #include <fstream>
 
+
 using namespace std;
 
 int main() {
+	int s = 0;
+	int d = 0;
 	string path;
-	fstream R;
+	fstream file;
 	char z;
+	string str;
 	int n = 0;
+	int el_am = 0;
+
+
 
 	cout << "Specify the full path to the text: " << endl;
 	cin >> path;
-	R.open(path, ios::in);
-	if (R) {
+	file.open(path, ios::in);
+	if (file) {
 
-		while (!R.eof()) {
+	
+		file >> str;
+		for (int i = 0; i < str.length(); i++) {
+			s = i + 1;
+			d = i + 2;
+			if (str[i] == '.' && str[s] == '.' && str[d] == '.') {
+				el_am++;
+			}
+		}
+	file.close();
+	}
 
-			R >> z;
+	file.open(path, ios::in);
+	if (file) {
+		while (!file.eof()) {
+
+			file >> z;
 			if (z == ','
-				|| z == '.' 
-				|| z == '-' 
-				|| z == '?' 
-				|| z == '"' 
-				|| z == '!' 
-				|| z == ';' 
-				|| z == ':' 
-				|| z == '...' 
-				|| z == '(' 
-				|| z == ')' ) {
+				|| z == '.'
+				|| z == '-'
+				|| z == '?'
+				|| z == '"'
+				|| z == '!'
+				|| z == ';'
+				|| z == ':'
+				|| z == '('
+				|| z == ')') {
+				z = 0;
 				n++;
 			}
 		}
-		R.close();
+	
+	file.close();
 
-		n == 0 ?
-			cout << '\n' << "There are no punctuation marks in the text" :
+		if (n == 0) {
+			cout << '\n' << "There are no punctuation marks in the text";
+		}
+		else if (el_am == 0)
 			cout << '\n' << "Result: " << n;
+		else {
+			n = n - 2 * el_am;
+			cout << '\n' << "Result: " << n;
+		}
 	}
 	else cout << "The file doesn't exist" << endl;
 	return 0;
